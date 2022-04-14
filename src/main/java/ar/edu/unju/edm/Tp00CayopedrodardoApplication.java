1314ed304c150baa7eb3633e82c5900047780c44
Dardo <Dardo@DESKTOP-V9NOLEN>
2022-04-14 11:58:57 -0300

    ProyectodeCalculadora

---
+++ src/main/java/ar/edu/unju/edm/Tp00CayopedrodardoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp00CayopedrodardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp00CayopedrodardoApplication.class, args);
+	}
+
+}
